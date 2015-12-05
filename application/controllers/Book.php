c304103edcb89a394d60a79bc15e54242fa58846
Muhammad A Azam <azzy.bu@gmail.com>
2015-12-05 19:20:53 +0600

    Book controller edited for modularity

--- application/controllers/Book.php
+++ application/controllers/Book.php
@@ -3,15 +3,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Book extends CI_Controller {
 
+    protected $view_subfolder = 'book';
+
     public function __construct(){
         parent::__construct();
-        $this->load->model('BookModel');
+        $this->load->model('BookModel','Model');
     }
 
     public function create(){
         $data = array(
             'title'=>'Create new Book',
-            'view_file' => 'book/create'
+            'view_file' => $this->view_subfolder.'/create'
         );
         $this->load->view('main_layout',$data);
     }
@@ -21,52 +23,52 @@ class Book extends CI_Controller {
             'author'=>$this->input->post('author'),
             'stock'=>$this->input->post('stock')
         );
-        $this->BookModel->save($data);
+        $this->Model->save($data);
         redirect(site_url('book'));
     }
 
     public function index(){
-        $books = $this->BookModel->getAll();
+        $books = $this->Model->getAll();
         $data = array(
             'title'=>'Books',
-            'view_file'=>'book/index',
+            'view_file'=>$this->view_subfolder.'/index',
             'books'=>$books
         );
         $this->load->view('main_layout',$data);
     }
 
-    public function show($book_id){
-        $book = $this->BookModel->getById($book_id);
+    public function show($id){
+        $book = $this->Model->getById($id);
         $data = array(
             'title' => 'Book | '.$book['id'],
-            'view_file' => 'book/show',
+            'view_file' => $this->view_subfolder.'/show',
             'book' => $book
         );
         $this->load->view('main_layout',$data);
     }
 
-    public function edit($book_id){
-        $old_details = $this->BookModel->getById($book_id);
+    public function edit($id){
+        $old_details = $this->Model->getById($id);
         $data = array(
             'title'=> 'Edit Book | '.$old_details['id'],
-            'view_file' => 'book/edit',
+            'view_file' => $this->view_subfolder.'/edit',
             'old_details' => $old_details
         );
         $this->load->view('main_layout',$data);
     }
 
-    public function update($book_id){
+    public function update($id){
         $data = array(
             'name' => $this->input->post('name'),
             'author' => $this->input->post('author'),
             'stock' => $this->input->post('stock')
         );
-        $this->BookModel->updateById($book_id,$data);
+        $this->Model->updateById($id,$data);
         redirect(site_url('book'));
     }
 
-    public function destroy($book_id){
-        $this->BookModel->deleteById($book_id);
+    public function destroy($id){
+        $this->Model->deleteById($id);
         redirect(site_url('book'));
     }
 }
