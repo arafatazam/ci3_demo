42c87f4688fc0a011c25b0d60322e2207e20bc62
Muhammad A Azam <azzy.bu@gmail.com>
2015-12-05 12:40:00 +0600

    Main layout added with the project

---
+++ application/views/main_layout.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+<div class="container">
+    <?php $this->load->view($view_file) ?>
+</div>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</body>
+</html>
