3ee62ca9760cabeb0005f9afe9934035baa97cf4
Muhammad A Azam <azzy.bu@gmail.com>
2015-12-05 20:03:09 +0600

    Home created by editing index function of Welcome controller

---
+++ application/views/home/index.php
@@ -0,0 +1,19 @@
+<h1>Welcome to My DB</h1>
+<table class="table table-bordered table-striped">
+    <thead>
+        <tr>
+            <th>Resource's Name</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Books</td>
+            <td><a class="btn btn-success" href="<?php echo site_url('book');?>">Go</a></td>
+        </tr>
+        <tr>
+            <td>Documentaries</td>
+            <td><a class="btn btn-success" href="<?php echo site_url('documentary');?>">Go</a></td>
+        </tr>
+    </tbody>
+</table>
